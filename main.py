f280506f5e22c7cc274baa0ea74f02b2f75399d1
Andrew Chapman <achapman@memanoth.net>
2025-03-14 21:33:57 +1100

    Ship and Asteroids Working

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     
         
     while True:
@@ -13,9 +29,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0, 0, 0))
+        updateable.update(dt)
+        screen.fill("black")
+        
+        for item in drawable:
+            item.draw(screen)
         
         pygame.display.flip()
+        
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
